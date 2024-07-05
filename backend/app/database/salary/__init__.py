56d4f049c66121f61313630481bb678c3b552937
satwikShresth <satwikshresth@gmail.com>
2024-07-05 15:31:42 -0400

    fixing the backend

--- backend/app/database/salary/__init__.py
+++ backend/app/database/salary/__init__.py
@@ -1,18 +1,73 @@
 from pathlib import Path
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import ForeignKey
+from sqlalchemy import Float
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, String, INTEGER
 
 DB_LOC = Path(__file__).resolve().parent / 'database.db'
 
+Base = declarative_base()
 
-class Salary(declarative_base()):
-    __tablename__ = 'salary'
 
-    Company = Column(String, primary_key=True, nullable=True)
-    Position = Column(String, primary_key=True, nullable=True)
-    Salary = Column(String, nullable=True)
-    Hours = Column(INTEGER, nullable=True)
-    Year = Column(INTEGER, nullable=True)
-    Major = Column(String, nullable=True)
-    Cycle = Column(String, nullable=True)
-    Experience = Column(INTEGER, nullable=True)
+class Company(Base):
+    __tablename__ = 'companies'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+
+
+class Position(Base):
+    __tablename__ = 'positions'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+
+
+class Major(Base):
+    __tablename__ = 'majors'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+
+
+class CoopCycle(Base):
+    __tablename__ = 'coop_cycles'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
+
+
+class CoopSalary(Base):
+    __tablename__ = 'coop_salaries'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    company_position_id = Column(
+        Integer,
+        ForeignKey('companies_positions.id'),
+        nullable=False
+    )
+
+    cycle_id = Column(
+        Integer,
+        ForeignKey('coop_cycles.id'),
+        nullable=False
+    )
+    salary_per_hour = Column(Float, nullable=False)
+    hours_per_week = Column(Integer, nullable=False)
+    experience = Column(Integer, nullable=False)
+    year = Column(Integer, nullable=False)
+
+
+class CompaniesPositions(Base):
+    __tablename__ = 'companies_positions'
+    __table_args__ = (
+        UniqueConstraint(
+            'company_id',
+            'position_id',
+            name='_company_position_uc'
+        ),
+    )
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    company_id = Column(Integer, ForeignKey('companies.id'), nullable=False)
+    position_id = Column(Integer, ForeignKey('positions.id'), nullable=False)
--- backend/app/sqlite.py
+++ backend/app/sqlite.py
@@ -1,73 +1,112 @@
-from typing import List, Dict, Any
-from abc import ABC
+from typing import List, Dict
+from abc import ABC, abstractmethod
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.ext.declarative import DeclarativeMeta
+from dataclasses import dataclass, asdict, fields
 from sqlalchemy.orm import sessionmaker
-from app.common.singleton import Singleton
 
-import app.database.uscities as uscities
+
+from app.common.singleton import Singleton
 import app.database.salary as salary
+import app.database.uscities as uscities
 
 
 class SqliteConn(Singleton, ABC):
-    db_path: str = None
-    schema: DeclarativeMeta = None
+    @abstractmethod
+    def __imports__(self):
+        pass
 
     def __init__(self):
+        self.__imports__()
         if self.db_path is None:
             raise ValueError(
-                "Database path must be set before creating a connection.")
+                "Database path must be set before creating a connection."
+            )
 
         self.engine = create_engine(f'sqlite:///{self.db_path}', echo=False)
         self.Session = sessionmaker(bind=self.engine)
         self.metadata = MetaData()
         self.metadata.reflect(bind=self.engine)
 
-    def fetchTableData(
-        self,
-        start: int = None,
-        end: int = None
-    ) -> List[Dict[str, Any]]:
-        with self.Session() as session:
-            query = session.query(self.schema)
-
-            if start is not None and end is not None:
-                query = query.offset(start).limit(end - start)
-
-            rows = query.all()
-            columns = self.schema.__table__.columns.keys()
-            data = [
-                dict(
-                    zip(columns, [getattr(row, col)for col in columns])
-                ) for row in rows
-            ]
-
-        return data
-
-    def fetchTableSize(
-        self,
-    ) -> int:
-        with self.Session() as session:
-            size = session.query(self.schema).count()
-        return size
-
-    def fetchTableHeaders(
-        self,
-    ) -> List[str]:
-        return [col.name for col in self.schema.__table__.columns]
-
     def __del__(self):
         self.engine.dispose()
 
 
 class SalaryDatabase(SqliteConn):
-    db_path = salary.DB_LOC
-    schema = salary.Salary
+
+    @dataclass(slots=True)
+    class CoopSalaryTable:
+        id: int
+        company: str
+        position: str
+        salary: float
+        hours: int
+        experience: int
+        year: int
+
+    def __imports__(self):
+        self.db_path = salary.DB_LOC
+
+    def fetchTableData(
+        self,
+        start: int = None,
+        end: int = None
+    ) -> List[Dict]:
+        with self.Session() as session:
+            query = session.query(
+                salary.CoopSalary.id,
+                salary.Company.name.label('company'),
+                salary.Position.title.label('position'),
+                salary.CoopSalary.salary_per_hour.label('salary'),
+                salary.CoopSalary.hours_per_week.label('hours'),
+                salary.CoopSalary.experience,
+                salary.CoopSalary.year
+            ).join(
+                salary.CompaniesPositions,
+                salary.CoopSalary.company_position_id == salary.CompaniesPositions.id
+            ).join(
+                salary.Company,
+                salary.CompaniesPositions.company_id == salary.Company.id
+            ).join(
+                salary.Position,
+                salary.CompaniesPositions.position_id == salary.Position.id
+            )
+
+            if start is not None:
+                query = query.offset(start)
+
+            if end is not None:
+                query = query.limit(end-start)
+
+            results = query.all()
+
+        return [
+            asdict(
+                self.CoopSalaryTable(*row)
+            ) for row in results
+        ]
+
+    def fetchTableSize(
+        self,
+    ) -> int:
+
+        with self.Session() as session:
+            size = session.query(salary.CoopSalary).count()
+        return size
+
+    def fetchTableHeaders(
+        self,
+    ) -> List[str]:
+
+        return [
+            field.name.capitalize() for field in fields(self.CoopSalaryTable)
+        ]
 
 
 class UsCitiesDatabase(SqliteConn):
-    db_path = uscities.DB_LOC
-    schema = uscities.UsCities
+
+    def __imports__(self):
+        self.db_path = uscities.DB_LOC
+        self.schema = uscities.UsCities
 
     def FetchStringMatchedData(self, query: str) -> List[Dict[str, str]]:
         with self.Session() as session:
@@ -86,3 +125,9 @@ class UsCitiesDatabase(SqliteConn):
                 "state_id": row.state_id
             } for row in results
         ]
+
+
+if __name__ == "__main__":
+    sal = SalaryDatabase()
+    print(sal.fetchTableSize())
+    print(sal.fetchTableData(0, 10))
